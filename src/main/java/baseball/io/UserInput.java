19ece6f68d73fe0d8392da0196f28e722204bb36
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2024-01-25 16:04:46 +0900

    [feat] done

---
+++ src/main/java/baseball/io/UserInput.java
@@ -0,0 +1,22 @@
+package baseball.io;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public String predict() throws IllegalArgumentException {
+        String rawInput = Console.readLine().trim();
+        if (rawInput.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
+        }
+        return rawInput;
+    }
+
+    public String end() throws IllegalArgumentException {
+        String rawInput = Console.readLine().trim();
+        if (rawInput.equals("1") || rawInput.equals("2")) {
+            return rawInput;
+        }
+        throw new IllegalArgumentException("1 혹은 2를 입력해야 합니다: " + rawInput);
+    }
+    
+}
